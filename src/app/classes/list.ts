ad29215abbf0890ee280a647ce058bc0dae3619f
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2017-12-13 20:40:00 -0500

    Classes and service ready

---
+++ src/app/classes/list.ts
@@ -0,0 +1,13 @@
+import { Item } from './item';
+
+export class List{
+    name:string;
+    finished:boolean;
+    items:Item[];
+
+    constructor(name:string){
+        this.name = name;
+        this.finished = false;
+    }
+
+}
