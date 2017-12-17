3da02facfd40c987e8f02c61aee03ba88663506c
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2017-12-17 12:46:14 -0500

    Detail component ready to use de routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule }       from '@angular/platform-browser';
+import { NgModule }            from '@angular/core';
+import { FormsModule }         from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { AppComponent }        from './app.component';
+import { HomeComponent }       from './components/home/home.component';
 import { UnfinishedComponent } from './components/unfinished/unfinished.component';
-import { FinishedComponent } from './components/finished/finished.component';
-import { ListService }       from './services/list.service';
-import { AddComponent }      from './components/add/add.component';
-import { PlaceholderPipe }   from './pipes/placeholder.pipe';
+import { FinishedComponent }   from './components/finished/finished.component';
+import { ListService }         from './services/list.service';
+import { AddComponent }        from './components/add/add.component';
+import { DetailComponent }     from './components/detail/detail.component';
+import { PlaceholderPipe }     from './pipes/placeholder.pipe';
+
 
 @NgModule({
   declarations: [
@@ -17,6 +19,7 @@ import { PlaceholderPipe }   from './pipes/placeholder.pipe';
     UnfinishedComponent,
     FinishedComponent,
     AddComponent,
+    DetailComponent,
     PlaceholderPipe
   ],
   imports: [
